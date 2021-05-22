e331ddec8f0cba0f6edbaa0ba671dcf46ed48978
zhabyuk <84640110+zhabyuk@users.noreply.github.com>
2021-05-22 16:08:47 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource “aws_instance” “mysecondec2” {
-    ami = “ami-048f6ed62451373d9”
-    instance_type = “t2.micro”
+resource "aws_instance" "mysecondec2" {
+    ami = "ami-048f6ed62451373d9"
+    instance_type = "t2.micro"
 }
